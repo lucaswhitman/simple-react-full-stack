34ffbc87a581ea85152950d2e7597b39e15d2899
lucaswhitman <lucasmichaelwhitman@gmail.com>
2020-01-17 15:24:35 -0700

    add server structure

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,12 @@
 import express = require('express');
 import os = require('os');
+import { userRouter } from './routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/users', userRouter);
 
 app.get('/api/getUsername', (req: any, res: { send: (arg0: { username: string; }) => any; }) => res.send({ username: os.userInfo().username }));
 
