3e32dfbe30ff191d5bb70897f1b16874703b2968
lucaswhitman <lucasmichaelwhitman@gmail.com>
2020-01-17 15:21:53 -0700

    initial server

---
+++ src/server/index.ts
@@ -0,0 +1,10 @@
+import express = require('express');
+import os = require('os');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/api/getUsername', (req: any, res: { send: (arg0: { username: string; }) => any; }) => res.send({ username: os.userInfo().username }));
+
+app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
