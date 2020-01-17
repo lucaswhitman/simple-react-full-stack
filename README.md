4837aa1cc4e3324dd78ab43107488e1886bbc3b9
lucaswhitman <lucasmichaelwhitman@gmail.com>
2020-01-17 15:21:19 -0700

    initial project

---
+++ README.md
@@ -0,0 +1,30 @@
+## Requirements
+- Node v13.2.0
+
+## Quick Start
+```bash
+#TODO: cleanup
+# Clone the repository
+git clone https://github.com/crsandeep/simple-react-full-stack
+
+# Go inside the directory
+cd simple-react-full-stack
+
+# Install dependencies
+yarn (or npm install)
+
+# Start development server
+yarn dev (or npm run dev)
+
+# Build for production
+yarn build (or npm run build)
+
+# Start production server
+yarn start (or npm start)
+```
+
+## Documentation
+
+### Folder Structure
+
+All the source code will be inside **src** directory. Inside src, there is client and server directory. All the frontend code (react, css, js and any other assets) will be in client directory. Backend Node.js/Express code will be in the server directory.
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const outputDirectory = 'dist';
+
+module.exports = {
+  entry: ['babel-polyfill', './src/client/index.tsx'],
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      exclude: /node_modules/,
+      test: /\.(t|j)sx?$/,
+      use: { loader: 'awesome-typescript-loader' }
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    },
+    {
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      loader: 'url-loader?limit=100000'
+    },
+    {
+      enforce: 'pre',
+      test: /\.(t|j)sx?$/,
+      loader: 'source-map-loader'
+    },
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx']
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': 'http://localhost:8080'
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin([outputDirectory]),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.ico'
+    })
+  ]
+};
